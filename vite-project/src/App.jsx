0028db63e9295e8ecf8f03db5d75abd295c12e35
Loloko <kowalczykmaciek220@gmail.com>
2023-12-28 21:23:58 +0100

    Advent of JavaScript#19

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [passwordLogic, setPasswordLogic] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(false);
+  const [confirmPasswordLogic, setConfirmPasswordLogic] = useState(false);
+  function validateEmail(email) {
+    const re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  const handleInputChange = (event) => {
+    if (event.target.name === "name") {
+      setName(event.target.value !== "" ? event.target.value : true);
+    }
+    if (event.target.name === "email") {
+      setEmail(validateEmail(event.target.value) ? event.target.value : true);
+    }
+    if (event.target.name === "password") {
+      let value = event.target.value;
+      setPasswordLogic(value);
+      const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])(.{8,})$/;
+      const isValid = passwordRegex.test(value);
+
+      setPassword(isValid ? value : true);
+
+      if (confirmPassword === value) {
+        setConfirmPassword(true);
+      }
+    }
+    if (event.target.name === "confirm-password") {
+      let value1 = event.target.value;
+      setConfirmPasswordLogic(value1);
+      setConfirmPassword(
+        event.target.value === password ? event.target.value : false
+      );
+    }
+  };
+  useEffect(() => {
+    checkpassword();
+  }, [passwordLogic, confirmPasswordLogic]);
+  const checkpassword = () => {
+    if (confirmPasswordLogic === passwordLogic) {
+      setConfirmPassword(true);
+      setPassword(false);
+    } else {
+      setConfirmPassword(false);
+    }
+  };
+
+  return (
+    <form action="">
+      <h1>Signup</h1>
+      <div className="field">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder=" "
+          onChange={handleInputChange}
+        />
+        <label for="name">Name</label>
+        {name === undefined || name === null || name === true ? (
+          <div className="error">
+            <img src="./error.svg" alt="Error" />A name is required
+          </div>
+        ) : name === "" ? null : (
+          <div className="success">
+            <img src="./success.svg" alt="Success" />
+          </div>
+        )}
+      </div>
+      <div className="field">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder=" "
+          onChange={handleInputChange}
+        />
+        <label for="email">Email</label>
+        {email === undefined || email === null || email === true ? (
+          <div className="error">
+            <img src="./error.svg" alt="Error" />
+            Email nie jest Prawidłowy
+          </div>
+        ) : email === "" ? null : (
+          <div className="success">
+            <img src="./success.svg" alt="Success" />
+          </div>
+        )}
+      </div>
+      <div className="field">
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder=" "
+          onChange={handleInputChange}
+        />
+        <label for="password">Password</label>
+        <button className="show-hide"></button>
+        {password === undefined || password === null || password === true ? (
+          <div className="error">
+            <img src="./error.svg" alt="Error" />
+            <span className="errorek">
+              Hasło nie ma co najmniej 8 znaków jedna dużą litera jedna cyfra
+              jeden znak specjalny (np. !@#$%^&*)
+            </span>
+          </div>
+        ) : password === "" ? null : (
+          <div className="success">
+            <img src="./success.svg" alt="Success" />
+          </div>
+        )}
+      </div>
+      <div className="field show">
+        <input
+          type="text"
+          name="confirm-password"
+          id="confirm-password"
+          placeholder=" "
+          onChange={handleInputChange}
+        />
+        <label for="confirm-password">Confirm Password</label>
+        <button className="show-hide"></button>
+        {confirmPassword === false ? (
+          <div className="error">
+            <img src="./error.svg" alt="Error" />
+            Hasła się nie zgadzają
+          </div>
+        ) : (
+          <div className="success">
+            <img src="./success.svg" alt="Success" />
+          </div>
+        )}
+      </div>
+      <div className="field">
+        <input type="submit" name="submit" value="Submit" />
+      </div>
+    </form>
+  );
+}
+
+export default App;
